9d0283435a6bf7bcea594c3d950cc2e71267809c
Gerson Noboa <gerson.noboa@glia.com>
2021-02-10 16:42:37 +0200

    Changes

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "SalemoveSDK",
-            url: "https://github.com/twilio/twilio-voice-ios/releases/download/6.2.1/TwilioVoice.xcframework.zip",
+            url: "https://github.com/gersonnoboa/glia-core/releases/download/1.0.0/SalemoveSDK.xcframework.zip",
             checksum: "da9594c6b50119663f4f7ea1b956e059df1e4dc9552e121ee21be2e2587b0e2d"
         )
     ]
