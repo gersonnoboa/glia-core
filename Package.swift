98646185d39f75f8d332808e9bab35a2f2d83517
Gerson Noboa <gerson.noboa@glia.com>
2021-02-18 16:08:33 +0200

    Slim SalemoveSDK

--- Package.swift
+++ Package.swift
@@ -4,20 +4,42 @@
 import PackageDescription
 
 let package = Package(
-    name: "SalemoveSDK",
+    name: "GliaCore",
     platforms: [
         .iOS(.v12)
     ],
     products: [
         .library(
-            name: "SalemoveSDK",
-            targets: ["SalemoveSDK"]),
+            name: "GliaCore",
+            targets: ["GliaCore"]),
+    ],
+    dependencies: [
+        .package(name: "Moya", url: "https://github.com/Moya/Moya.git", .exact("14.0.0")),
+        .package(name: "Macaw", url: "https://github.com/exyte/Macaw.git", .exact("0.9.7")),
+        .package(name: "ReactiveSwift", url: "https://github.com/ReactiveCocoa/ReactiveSwift.git", .exact( "6.5.0")),
+        .package(name: "SocketIO", url: "https://github.com/salemove/ios-socket-client", .exact( "9.2.0")),
+        .package(name: "SwiftPhoenixClient", url: "https://github.com/davidstump/SwiftPhoenixClient.git", .exact("1.2.1")),
+        .package(name: "TwilioVoice", url: "https://github.com/twilio/twilio-voice-ios", .exact( "6.2.0")),
+        .package(name: "WebRTC", url: "https://github.com/alexpiezo/WebRTC.git", .exact( "1.1.31567")),
     ],
     targets: [
         .binaryTarget(
-            name: "SalemoveSDK",
+            name: "SalemoveSDKZip",
             url: "https://github.com/gersonnoboa/glia-core/releases/download/1.0.2/SalemoveSDK.xcframework.zip",
             checksum: "10ce347b2c3e127cd78d3da68cf879ce075a16c9c1c14d404392decbcee80b58"
+        ),
+        .target(
+            name: "GliaCore",
+            dependencies: [
+                "SalemoveSDKZip",
+                "Moya",
+                "Macaw",
+                "ReactiveSwift",
+                "SocketIO",
+                "SwiftPhoenixClient",
+                "TwilioVoice",
+                "WebRTC"
+            ]
         )
     ]
 )
