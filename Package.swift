40d721fe1b4582f3aff1034dd3241b254e4e98cb
Gerson Noboa <gerson.noboa@glia.com>
2021-04-30 11:41:37 +0300

    Updates

--- Package.swift
+++ Package.swift
@@ -14,48 +14,6 @@ let package = Package(
             targets: ["SalemoveSDKTarget"]
         )
     ],
-    dependencies: [
-        .package(
-            name: "Macaw",
-            url: "https://github.com/exyte/Macaw.git",
-            .upToNextMajor(from: "0.9.7")
-        ),
-        .package(
-            name: "Moya",
-            url: "https://github.com/Moya/Moya.git",
-            .upToNextMajor(from: "14.0.1")
-        ),
-        .package(
-            name: "ReactiveSwift",
-            url: "https://github.com/ReactiveCocoa/ReactiveSwift.git",
-            .upToNextMajor(from: "6.6.0")
-        ),
-        .package(
-            name: "SocketIO",
-            url: "https://github.com/socketio/socket.io-client-swift.git",
-            .upToNextMajor(from: "15.2.0")
-        ),
-        .package(
-            name: "SwiftPhoenixClient",
-            url: "https://github.com/davidstump/SwiftPhoenixClient.git",
-            .upToNextMajor(from: "1.2.0")
-        ),
-        .package(
-            name: "SWXMLHash",
-            url: "https://github.com/drmohundro/SWXMLHash.git",
-            .upToNextMajor(from: "5.0.0")
-        ),
-        .package(
-            name: "TwilioVoice",
-            url: "https://github.com/gersonnoboa/twilio-voice-ios.git",
-            .upToNextMajor(from: "1.0.0")
-        ),
-        .package(
-            name: "WebRTC",
-            url: "https://github.com/gersonnoboa/spm-test.git",
-            .upToNextMajor(from: "1.0.4")
-        )
-    ],
     targets: [
         .binaryTarget(
             name: "SalemoveSDK",
@@ -65,15 +23,7 @@ let package = Package(
         .target(
             name: "SalemoveSDKTarget",
             dependencies: [
-                "SalemoveSDK",
-                "Moya",
-                "Macaw",
-                "ReactiveSwift",
-                "SocketIO",
-                "SwiftPhoenixClient",
-                "SWXMLHash",
-                "TwilioVoice",
-                "WebRTC"
+                "SalemoveSDK"
             ]
         )
     ]
